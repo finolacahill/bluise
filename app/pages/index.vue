cc2cc7e2d4bf8d595e7ff3c5aff2ac63a85294e4
Finola Cahill <finola.cahill@intel.com>
2023-10-17 08:50:25 +0100

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,6 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-        
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
         <img
@@ -21,7 +20,6 @@ import { Component, Vue } from 'nuxt-property-decorator';
 import settings from '@/content/settings/general.json';
 
 @Component({
-  // Called to know which transition to apply
   transition() {
     return 'slide-left';
   },
@@ -29,6 +27,31 @@ import settings from '@/content/settings/general.json';
 export default class Home extends Vue {
   welcomeText = settings.welcomeText;
   isSignedUp = false;
+
+  async asyncData({ app }) {
+    const pageTitle = 'Finola Cahill Writer & Musician'; // Replace with your page title
+    const pageDescription = 'Finola Cahill is an Irish poet, writer and musician from Co. Mayo in the west of Ireland. She is a member of Pembroke with Ellen Quinn Banville'; // Replace with your meta description
+    
+    // Set meta tags dynamically
+    app.head = {
+      title: Finola Cahill Writer &  Musician,
+      meta: [
+        {
+          hid: 'Finola Cahill is an Irish poet, writer and musician from Co. Mayo in the west of Ireland. She is a member of Pembroke with Ellen Quinn Banville',
+          name: 'Finola Cahill is an Irish poet, writer and musician from Co. Mayo in the west of Ireland. She is a member of Pembroke with Ellen Quinn Banville',
+          content: Finola Cahill is an Irish poet, writer and musician from Co. Mayo in the west of Ireland. She is a member of Pembroke with Ellen Quinn Banville,
+        },
+        // Add more meta tags if needed
+      ],
+    };
+
+    return {
+      // Your asyncData logic if any
+    };
+  }
 }
 </script>
 
+<style lang="scss" scoped>
+/* Your component-specific styles */
+</style>
